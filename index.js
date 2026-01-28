250a9bb6c37978da906f2f7b021663187da84a11
smreaz12 <smreaz555@gmail.com>
2026-01-28 11:25:07 +0600

    Object in JS

---
+++ index.js
@@ -0,0 +1,29 @@
+//object//
+
+const phone ={
+    brand: "Apple",
+    model: "17 pro max",
+    color: "White",
+}
+
+console.log(phone);
+
+
+//
+
+const laptop={
+    brand: "Apple",
+    model: "Macbook m5",
+    display: '15"',
+    battery: 20000+'mah',
+
+    performance:{
+        ram: "12GB",
+        rom: "512GB",
+        chip: "M5",
+        display_quality: "2k",
+    }
+}
+
+
+console.log(laptop);
