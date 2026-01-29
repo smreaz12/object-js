8d2a832b11a6ac634216f1e2f5bb385447ed3786
smreaz12 <smreaz555@gmail.com>
2026-01-29 11:11:04 +0600

    get key & values in js

---
+++ key-value.js
@@ -0,0 +1,16 @@
+const computer = {
+    brand: "Lenovo",
+    processor: "AMD",
+    motherbord: "Gigabyte",
+    ram: "16GB",
+    nvme: "512GB",
+    monitor: 'Pc Power 22"'
+}
+
+//----Get Keys----//
+const keys = Object.keys(computer);
+console.log(keys);
+
+//----Get values----//
+const values = Object.values(computer);
+console.log(values);
